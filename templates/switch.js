f46fb6daebf7fb39d510ba5f5e1dac689253455d
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-01-13 21:12:02 +0500

    test 2

--- templates/switch.js
+++ templates/switch.js
@@ -1,17 +1,14 @@
 function openTab(evt, tabName) {
-    // Получаем все элементы с классом "tabcontent" и скрываем их
     var tabcontent = document.getElementsByClassName("tabcontent");
     for (var i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
 
-    // Получаем все элементы с классом "tablinks" и удаляем класс "active"
     var tablinks = document.getElementsByClassName("tablinks");
     for (var i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Отображаем текущую вкладку и добавляем класс "active" к кнопке
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
